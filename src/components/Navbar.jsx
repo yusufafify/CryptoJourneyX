4296cab6f5d524dd3b704e1c0abc2b229f881d07
yusufafify <youssef.mansour02@eng-st.cu.edu.eg>
2023-09-22 19:30:03 +0300

    changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -38,7 +38,7 @@ const Navbar = () => {
       <div className="logo-container">
         <Avatar src={icon} size="large" />
         <Typography.Title level={2} className="logo">
-          <Link to="/">CryptoPedia</Link>
+          <Link to="/">Cryptopedia-Hub</Link>
         </Typography.Title>
         <Button
           className="menu-control-container"
